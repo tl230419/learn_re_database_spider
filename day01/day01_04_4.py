3f64ffda1806ddf884561c1dcd93317bc675a2f3
Allen <tl230419@163.com>
2020-04-30 02:18:28 +0800

    update logic

---
+++ day01/day01_04_4.py
@@ -0,0 +1,18 @@
+'''
+*****************
+Date: 2020-04-30
+Author: Allen
+*****************
+'''
+
+import re
+
+tels = ["13100001234", "18912344321", "10086", "18800007777"]
+
+for tel in tels:
+    ret = re.match("1\d{9}[0-35-68-9]", tel)
+    if ret:
+        print(ret.group())
+    else:
+        print("%s 不是想要的手机号" % tel)
+
